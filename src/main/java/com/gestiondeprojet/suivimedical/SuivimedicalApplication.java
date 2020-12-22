acda68d0f2555aa4bdbc942cee8fdb13736d360b
Touri Mohamed Amine <touri.med.amine@gmail.com>
2020-12-22 23:49:49 +0100

    Start Spring Initializr

---
+++ src/main/java/com/gestiondeprojet/suivimedical/SuivimedicalApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondeprojet.suivimedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuivimedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuivimedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiondeprojet/suivimedical/SuivimedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondeprojet.suivimedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuivimedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
